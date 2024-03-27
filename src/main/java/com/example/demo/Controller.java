f1aabb75af8fc5986cccf34cc60b7e51b851b2aa
Jannik Luethi <jannik.luethi@swisscom.com>
2024-03-27 16:50:10 +0100

    -

--- src/main/java/com/example/demo/Controller.java
+++ src/main/java/com/example/demo/Controller.java
@@ -2,6 +2,7 @@ package com.example.demo;
 import java.util.List;
 import java.util.Optional;
 
+import org.springframework.web.bind.annotation.CrossOrigin;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestParam;
@@ -10,6 +11,7 @@ import org.springframework.web.bind.annotation.RestController;
 import com.example.demo.Models.Service;
 import com.fasterxml.jackson.databind.ObjectMapper;
 
+@CrossOrigin(origins = "http://localhost:4200")
 @RestController
 public class Controller {
 
